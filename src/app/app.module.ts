293c62434539ec3916bc08e21c8c3bf3c6a995b4
qqvvkk <l.delvecchio@hotmail.com>
2022-06-11 20:47:42 +0200

    added main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoBoxComponent } from './info-box/info-box.component';
+import { SceneComponent } from './scene/scene.component';
+import { TextAreaComponent } from './text-area/text-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoBoxComponent,
+    SceneComponent,
+    TextAreaComponent
   ],
   imports: [
     BrowserModule,
