322e01063c22212575ce1146115a9e055fd6a8f6
qqvvkk <l.delvecchio@hotmail.com>
2022-06-14 13:45:24 +0200

    Revert "update text area"
    This reverts commit 2cb4968934c7e94eda9dc7f993c856d6f871f307.

--- src/app/model/Run.ts
+++ src/app/model/Run.ts
@@ -9,10 +9,21 @@ export class Run {
     stage: Stage;
     time: number;               // TODO work with time data
 
+    /*constructor (player: Character,
+        party: [],
+        level: number,
+        stage: Stage,
+        time: number) {
+            this.player = player;
+            this.party =  party;
+            this.level = level;
+            this.stage = stage;
+            this.time = time;
+        }*/
     constructor() {
         this.player = new Character();
         this.party = [];
-        this.level = 0;
+        this.level = 9;
         this.stage = new Stage();
         this.time = 0;
     }
--- src/app/text-area/text-area.component.ts
+++ src/app/text-area/text-area.component.ts
@@ -1,11 +1,11 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { OnInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-text-area',
   templateUrl: './text-area.component.html',
   styleUrls: ['./text-area.component.scss']
 })
-export class TextAreaComponent implements AfterViewInit {
+export class TextAreaComponent implements OnInit {
 
   @ViewChild('textArea') textArea: ElementRef;
 
@@ -13,17 +13,17 @@ export class TextAreaComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngAfterViewInit(): void {
-    this.textArea.nativeElement.addEventListener('change', this.focus());
+  ngOnInit(): void {
   }
 
   pushText(text: string) {
     this.currentText = this.currentText + "&#10;" + text;
-    //this.textArea.nativeElement.setSelectionRange(0, 0);
+    //
   }
 
   focus() {
     this.textArea.nativeElement.focus();
+    this.textArea.nativeElement.setSelectionRange(0, 0);
   }
 
 }
