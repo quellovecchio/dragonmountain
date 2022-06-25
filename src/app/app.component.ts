ad58fd0028a09517e10fa8150cd52d416ed3060d
qqvvkk <l.delvecchio@hotmail.com>
2022-06-25 19:25:51 +0200

    game intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,29 +14,37 @@ export class AppComponent {
   @ViewChild(ViewportComponent)
   viewport: ViewportComponent;
 
+  loadedSavedData: boolean = false;
+  textSpeed: number = 1;
   run: Run;
 
   constructor(viewport: ViewportComponent) {
     this.viewport = viewport;
     this.run = this.generateTestRun();
     this.viewport.refreshInfoBox(this.run);
-    this.viewport.refreshScene(this.run);
-    setInterval(() => {
-      this.viewport.pushText(Math.round(Math.random() * 1000).toString());
-      this.viewport.refreshScene(this.run);
-    }, 600);
+    if(!this.loadedSavedData) {
+      this.greetPlayer();
+    }
   }
-
+  
   generateTestRun(): Run {
     console.log("generateTestRun() - start");
-    this.viewport.pushText("Generating run...");
     let newRun = new Run();
     for(var i = 0; i < 5; i++){
       newRun.stage.locations.push(new Location());
     }
-    this.viewport.pushText("Run loaded.");
     console.log("generateTestRun() - end");
     return newRun;
   }
+
+  
+  greetPlayer() {
+    var that = this;
+    setTimeout(function() { that.viewport.pushText("The first Stage of your journey is " + that.run.stage.name + "...") }, 1000 * that.textSpeed);
+    setTimeout(function() { that.viewport.pushText("And it's full of Locations you can Explore!") }, 2000 * that.textSpeed);
+    setTimeout(function() { that.viewport.pushText("What is our first destination?") }, 3000 * that.textSpeed);
+    setTimeout(function() { that.viewport.refreshScene(that.run) }, 4000 * that.textSpeed);
+  }
+
 }
 
