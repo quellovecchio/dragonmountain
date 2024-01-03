5755bf17498221fd0db02e59701535cd94eee591
qqvvkk <l.delvecchio@hotmail.com>
2024-01-03 18:11:22 +0100

    base actor update

--- src/app/model/Actors/Actor.ts
+++ src/app/model/Actors/Actor.ts
@@ -6,7 +6,7 @@ import { Item } from "../items/Item";
 export class Actor {
 
     imagePath: string = "/assets/images/actor1.png";
-    name: string = "test_actor";
+    name: string = "Hero";
     level: number = 1;
     loot: Item[] = [];
     // todo gives item when interacted
@@ -21,7 +21,7 @@ export class Actor {
     equipment: Equip[] = [];
     // an interaction can proc landing in the room, winning a fight against him or giving the neededItem and can make the npc vanish
     interactions: Interaction[] = [];
-    dialogue: string = '';
+    dialogue: string[] = [''];
 
     constructor() {
     }
