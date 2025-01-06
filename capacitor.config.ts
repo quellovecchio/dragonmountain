08c9ff1d8549c9631d95466fa994e8d48288f858
qqvvkk <l.delvecchio@hotmail.com>
2025-01-06 20:13:24 +0100

    capacitor impl

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.qqvvkk.dragonmountain',
+  appName: 'DragonMountain',
+  webDir: 'dist/dragon-mountain',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
