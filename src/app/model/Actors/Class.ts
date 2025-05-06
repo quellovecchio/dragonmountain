7b839cc0bb79672199b1bb5ca3faa70e1569aeec
qqvvkk <l.delvecchio@hotmail.com>
2025-05-06 14:08:26 +0200

    attack range impl

--- src/app/model/Actors/Class.ts
+++ src/app/model/Actors/Class.ts
@@ -5,6 +5,8 @@ export class Class {
     id: number = 0;
     name: string = 'class';
     skillTree: {skillId: number, unlockLevel: number, unlockStat: string}[] = [];
+    // range of the basic attack: 0 is meelee only
+    attackRange: number = 0;
 
     constructor() {
         this.id = idCounter++;
